68e8af4fd4d7c51283d13dff0d296a99df4f49f9
Josh Renaud <josh.renaud41@gmail.com>
2021-06-18 15:17:37 -0500

    changed timer script, added placeholder questions, first function for questions

--- script.js
+++ script.js
@@ -2,6 +2,32 @@
 
 const questions = [
 
+    {
+        question: "What is the term used for a score of 2 below par on any given hole?",
+        answers: ["a) Birdie", "b) Eagle", "c) Albatross", "d) Bogey"],
+        correct: "b) Eagle"
+    },
+    {
+        question: "What is the term used for a score of 2 below par on any given hole?",
+        answers: ["a) Birdie", "b) Eagle", "c) Albatross", "d) Bogey"],
+        correct: "b) Eagle"
+    },
+    {
+        question: "What is the term used for a score of 2 below par on any given hole?",
+        answers: ["a) Birdie", "b) Eagle", "c) Albatross", "d) Bogey"],
+        correct: "b) Eagle"
+    },
+    {
+        question: "What is the term used for a score of 2 below par on any given hole?",
+        answers: ["a) Birdie", "b) Eagle", "c) Albatross", "d) Bogey"],
+        correct: "b) Eagle"
+    },
+    {
+        question: "What is the term used for a score of 2 below par on any given hole?",
+        answers: ["a) Birdie", "b) Eagle", "c) Albatross", "d) Bogey"],
+        correct: "b) Eagle"
+    },
+     
 
 ]
 
@@ -9,7 +35,7 @@ const questions = [
 
 //.. Link variables to HTML Elements
 
-var timer = document.getElementById("timer");
+var timer = document.getElementById("#timer");
 var remainingTime = document.getElementById("remainingTime");
 var timeGone = document.getElementById("timeGone");
 var startButton = document.getElementById("startButton");
@@ -24,8 +50,6 @@ var quiz = document.getElementById("quiz");
 var quizContainer = document.getElementById("container");
 
 
-var questionIndex = 0;
-
 
 
 
@@ -34,35 +58,47 @@ var questionIndex = 0;
 
 
 //... Functions for timer, presenting questions, checking answers
-
-var totalTime = 91;
-
-function newQuiz() {
-    questionIndex = 0;
-    totalTime = 90;
-    remainingTime.textContent = totalTime;
-
-    startButton.style.display = "none";
-    quizContainer.style.display = "block";
-    timer.style.display = "block";
-    timeGone.style.display = "none";
-
-    var startTimer = setInterval(function() {
-        totalTime--;
-        remainingTime.textContent = totalTime;
-        if(totalTime <= 0) {
-            clearInterval(startTimer);
-            if (questionIndex < questions.length - 1) {
-                gameOver();
-            }
-        }
-    },1000);
-
-    showQuiz();
-};
+startButton.addEventListener("click", function(){
+    var timer = setInterval(startTimer,1000);
+})
 
 
-startButton.addEventListener("click", newQuiz);
+function askQuestion() {
+    questionHeader.textContent = questions[questionIndex].question;
+    choiceA.textContent = questions[questionIndex].choices[0];
+    choiceB.textContent = questions[questionIndex].choices[1];
+    choiceC.textContent = questions[questionIndex].choices[2];
+    choiceD.textContent = questions[questionIndex].choices[3];
+}
+
+
+var timeLeft = 90;
+
+function startTimer() { 
+    timeLeft --;
+    if (timeLeft < 0)
+    {
+        clearInterval(startTimer);
+        return;
+    }
+    document.getElementById("#timer").innerHTML = timeLeft + " secs";
+}
+
+
+
+
+
+
+
+
+function beginQuiz() {
+    showQuestion();
+}
+
+
+
+
+
 
 
 
