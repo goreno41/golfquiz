234bb9f3591ae2abd3e88a766cd258be124868c5
Josh Renaud <josh.renaud41@gmail.com>
2021-06-18 10:49:25 -0500

    added timer elements in HTML, linked to JS sheet with elements

---
+++ script.js
@@ -0,0 +1,27 @@
+//.. Define a set of questions
+
+const questions = [
+
+
+]
+
+
+
+//.. Link variables to HTML Elements
+
+var timer = document.getElementById("timer");
+var remainingTime = document.getElementById("remainingTime");
+var timeGone = document.getElementById("timeGone");
+
+
+
+
+
+//... Functions for timer, presenting questions, checking answers
+
+
+
+
+
+
+
