359709f8d6145522f143ae202d9c2037dfb2cf7f
Josh Renaud <josh.renaud41@gmail.com>
2021-06-18 11:08:40 -0500

    added answer buttons and question header, linked to JS file with variables

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ const questions = [
 var timer = document.getElementById("timer");
 var remainingTime = document.getElementById("remainingTime");
 var timeGone = document.getElementById("timeGone");
+var startButton = document.getElementById("startButton");
+var quiz = document.getElementById("quiz");
+var questionHeader = document.getElementById("questionHeader");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+
+
 
 
 
