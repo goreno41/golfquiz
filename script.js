43f26a200d09caaaa0c6be0b7875731bea4b226a
Josh Renaud <josh.renaud41@gmail.com>
2021-06-18 11:30:03 -0500

    added timer function, event listener

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ var answer1 = document.getElementById("answer1");
 var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
+var startButton = document.getElementById("startButton");
+var quiz = document.getElementById("quiz");
+var quizContainer = document.getElementById("container");
+
+
+var questionIndex = 0;
+
 
 
 
@@ -28,7 +35,34 @@ var answer4 = document.getElementById("answer4");
 
 //... Functions for timer, presenting questions, checking answers
 
+var totalTime = 91;
 
+function newQuiz() {
+    questionIndex = 0;
+    totalTime = 90;
+    remainingTime.textContent = totalTime;
+
+    startButton.style.display = "none";
+    quizContainer.style.display = "block";
+    timer.style.display = "block";
+    timeGone.style.display = "none";
+
+    var startTimer = setInterval(function() {
+        totalTime--;
+        remainingTime.textContent = totalTime;
+        if(totalTime <= 0) {
+            clearInterval(startTimer);
+            if (questionIndex < questions.length - 1) {
+                gameOver();
+            }
+        }
+    },1000);
+
+    showQuiz();
+};
+
+
+startButton.addEventListener("click", newQuiz);
 
 
 
